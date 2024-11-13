5b6a6b996a822e9dab3a9f39d4234381abed5ba9
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-11-12 19:50:02 -0800

    Update buffer.cpp

--- assign4/buffer.cpp
+++ assign4/buffer.cpp
@@ -1,38 +1,34 @@
 /**
 * Assignment 4: Producer Consumer Problem
- * @file buffer.h
+ * @file buffer.cpp
  * @author Erin Bailey and Zach Miller
- * @brief header file for the buffer class
+ * @brief Implementation file for the buffer class
  * @version 0.1
  */
 
-#ifndef ASSIGN4_BUFFER_H
-#define ASSIGN4_BUFFER_H
+// You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
+// Remember to add sufficient and clear comments to your code
+#include "buffer.h"
+#include <iostream>
 
-// Define the data type of the buffer items
-typedef int buffer_item;
 
 /**
- * @brief The bounded buffer class. The number of items in the buffer cannot exceed the size of the buffer.
- */
-class Buffer {
-private:
-    int size,
-        front,
-        back;
-    int *buffer;
-
-public:
-    /**
      * @brief Construct a new Buffer object
      * @param size the size of the buffer
      */
-    Buffer(int size = 5);
+    Buffer::Buffer(int size = 5){
+        this->size  = size;
+        front = 0; // front is where items are to be inserted in the buffer
+        back = 0;  // back is where items are to be removed from
+        buffer = new int[size];
+    }
 
     /**
      * @brief Destroy the Buffer object
      */
-    ~Buffer();
+    Buffer::~Buffer(){
+        delete buffer;
+    }
 
     /**
      * @brief Insert an item into the buffer
@@ -40,7 +36,13 @@ public:
      * @return true if successful
      * @return false if not successful
      */
-    bool insert_item(buffer_item item);
+    bool Buffer::insert_item(buffer_item item){
+        if(front != mod(back - 1, size)){//buffer not empty
+            buffer[front] = item;
+            front = (front + 1) % size;
+            return true;
+        }else return false;
+    }
 
     /**
      * @brief Remove an item from the buffer
@@ -48,42 +50,72 @@ public:
      * @return true if successful
      * @return false if not successful
      */
-    bool remove_item(buffer_item *item);
+    bool Buffer::remove_item(buffer_item *item){
+        if(front != back){
+            *item = buffer[back];
+            back = (back + 1) % size;
+            return true;
+        }else return false;
+
+    }
 
     /**
      * @brief Get the size of the buffer
      * @return the size of the buffer
      */
-    int get_size();
+    int Buffer::get_size(){
+        return size;
+
+    }
 
     /**
      * @brief Get the number of items in the buffer
      * @return the number of items in the buffer
      */
-    int get_count();
+    int Buffer::get_count(){
+        //cases
+        //1: count = 0
+        //2: front > back
+        //3: front < back
+
+        //case 1
+        if(front == back){
+            return 0;
+        }else
+        if(front > back){
+            return front - back;
+        }else{//front < back
+            return size - (back - front);
+        }
+    }
 
     /**
      * @brief Chceck if the buffer is empty
      * @return true if the buffer is empty, else false
      */
-    bool is_empty();
+    bool Buffer::is_empty(){
+        if(front == back) return true;
+        else return false;
+    }
     /**
      * @brief Check if the buffer is full
      * @return true if the buffer is full, else false
      */
-    bool is_full();
+    bool Buffer::is_full(){
+        if(front == mod(back - 1, size)) return true;
+        else return false;
+    }
 
     /**
      * @brief Print the buffer
      */
-    void print_buffer();
-
-    //c++ % doesn't return positive values for negatives mod n, this fixes that
-    int mod(int a, int n){
-        while(a < 0){
-            a += n;
+    void Buffer::print_buffer(){
+        std::cout << "Buffer: [";
+        for(int i = 0; i < size; i++){
+            std::cout << buffer[i];
+            if(i != size - 1) std::cout << ", ";
         }
-        return a % n;
+        std::cout << "]" << std::endl;
     }
-};
-#endif //ASSIGN4_BUFFER_H
+// TODO: Add your implementation of the buffer class here
+
