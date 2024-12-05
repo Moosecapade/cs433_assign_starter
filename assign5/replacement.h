4ed31502f4996bdf431fe244daa9a678279c47e9
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-12-04 16:30:35 -0800

    Update replacement.h

--- assign5/replacement.h
+++ assign5/replacement.h
@@ -1,11 +1,10 @@
 /**
 * Assignment 5: Page replacement algorithms
  * @file replacement.h
- * @author ??? (TODO: your name)
+ * @author Zach Miller and Erin Bailey
  * @brief A base class for different page replacement algorithms.
  * @version 0.1
  */
-//You must complete the all parts marked as "TODO". Delete "TODO" after you are done.
 // Remember to add sufficient and clear comments to your code
 #pragma once
 
