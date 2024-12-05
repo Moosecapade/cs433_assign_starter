b51d9893bfa1de5a6359dbbe0e52069ce69a99d3
Erin Bailey <146312546+Moosecapade@users.noreply.github.com>
2024-12-04 18:42:32 -0800

    Update replacement.cpp

--- assign5/replacement.cpp
+++ assign5/replacement.cpp
@@ -16,7 +16,8 @@
 Replacement::Replacement(int num_pages, int num_frames)
 : page_table(num_pages)
 {
-	//TODO: Add your implementation here
+	this->num_pages = num_pages;
+    this->num_frames = num_frames;
 }
 
 // Destructor
@@ -27,19 +28,39 @@ Replacement::~Replacement()
 
 // Simulate a single page access 
 // @return true if it's a page fault
-bool Replacement::access_page(int page_num, bool is_write)
+bool Replacement::access_page(int page_num, bool is_write)//all mem accesses are
 {
-    // TODO: Add your implementation here
     // If the page is valid, it calls the touch_page function. 
     // If the page is not valid but free frames are available, it calls the load_page function.
     // If the page is not valid and there is no free frame, it calls the replace_page function.
+    
+
+    PageEntry* apage = &page_table[page_num];
+
+    if(apage->valid == false){
+        //page is not valid
+        if(num_frames > used_frames){
+            //free frames exist
+            //call load_page
+        }else{
+            //no free frames exist
+            //call replace_page
+        }
+
+    }else {
+        //page IS valid
+        //call touch_page
+    }
+
+    counter++;
+
     return false;
 }
 
 // Print out statistics of simulation
 void Replacement::print_statistics() const {
         // TODO: print out the number of references, number of page faults and number of page replacements
-		std::cout << "Number of references: \t\t"  << std::endl;
-		std::cout << "Number of page faults: \t\t" << std::endl;
-		std::cout << "Number of page replacements: \t"  << std::endl;
+		std::cout << "Number of references: \t\t"  << num_refs << std::endl;
+		std::cout << "Number of page faults: \t\t" << num_fault << std::endl;
+		std::cout << "Number of page replacements: \t"  << num_replace << std::endl;
 }
